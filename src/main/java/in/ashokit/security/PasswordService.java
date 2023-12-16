f0775c0b978b48531362c358ffc750f176d6ca4c
rkb03 <ranjan.styleplay@gmail.com>
2023-12-16 11:01:18 +0530

    removed unwanted code

--- src/main/java/in/ashokit/security/PasswordService.java
+++ src/main/java/in/ashokit/security/PasswordService.java
@@ -6,8 +6,6 @@ import java.util.Base64.Encoder;
 
 public class PasswordService {
 	
-	int i = 10;
-
 	public static String encode(String txt) {
 		Encoder encoder = Base64.getEncoder();
 		return encoder.encodeToString(txt.getBytes());
